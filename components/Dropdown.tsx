39d3458baf8115046ba325160d1f8f73694c81d6
jeffrey-arias <jeffrey.e.arias@gmail.com>
2023-12-27 21:41:54 +0800

    feat: dropdown button - support more functionalities

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -6,6 +6,8 @@ import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import { StyledCheckbox } from './StyledCheckbox'
 import { ListItemText } from '@mui/material'
+import { TextInput } from './TextInput'
+import { join } from 'path'
 
 type DropdownProps = {
   label?: string
@@ -16,6 +18,9 @@ type DropdownProps = {
   className?: string
   fullWidth?: boolean
   multiple?: boolean
+  customInput?: boolean
+  renderCheckbox?: boolean
+  placeholder?: string
 }
 export function Dropdown(props: DropdownProps) {
   const {
@@ -26,29 +31,65 @@ export function Dropdown(props: DropdownProps) {
     disabled,
     className,
     fullWidth,
-    multiple
+    multiple,
+    customInput,
+    renderCheckbox,
+    placeholder
   } = props
+
+  // Remove highlight when option is selected, and add checkbox instead
+  // Also add option to Select/Deselect All options
+  const [localOptions, setLocalOptions] = useState<any[]>(options)
+  const [textFieldValue, setTextFieldValue] = useState<string>('')
+
+  const handleAddMenuOptions = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    event.stopPropagation()
+    const {
+      target: { value }
+    } = event as any
+    if (event.code === 'Enter' && value?.trim()?.length > 0) {
+      setLocalOptions([...localOptions, { value: value, label: value }])
+    }
+  }
 
   const handleChange = (event: SelectChangeEvent) => {
     if (setSelected) {
-      setSelected(event.target.value)
+      const {
+        target: { value }
+      } = event
+      setSelected(typeof value === 'string' ? value.split(',') : value)
     }
   }
   return (
     <FormControl size='small' className={`${fullWidth ? 'w-full' : ''}`}>
-      <InputLabel className='m-0 p-0 text-[14px]'>{label}</InputLabel>
+      <InputLabel className='text-[14px]'>{label ?? placeholder}</InputLabel>
+
       <Select
         value={selected ?? []}
-        label={label}
+        label={label ?? placeholder}
         onChange={handleChange}
         size='small'
-        className={`min-w-[160px] ${
+        className={`min-w-[160px] truncate ${
           fullWidth ? 'w-full' : 'w-[160px]'
         } ${className}`}
         sx={{
           padding: 0,
           '.MuiOutlinedInput-notchedOutline': {
             borderColor: 'black'
+          },
+          '& .MuiSelect-select': {
+            display: 'block',
+            verticalAlign: 'middle',
+            textAlign: 'left',
+            justifyItems: 'center',
+            alignItems: 'center',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+            lineHeight: '36px',
+            height: '36px'
           }
         }}
         disabled={disabled}
@@ -56,13 +97,47 @@ export function Dropdown(props: DropdownProps) {
           style: { paddingTop: 0, paddingBottom: 0, height: '36px' }
         }}
         multiple={multiple}
+        renderValue={(selected) => {
+          if (selected.length === 0) {
+            return <em>Placeholder</em>
+          }
+
+          return typeof selected === 'string'
+            ? options.find((i) => i.value === selected)?.label
+            : // : (selected as Array<any>)?.map(
+              //     (s, index) =>
+              //       options.find((i) => i.value === s)?.label +
+              //       `${index === (selected as any)?.length ? '' : ', '}`
+              //   )
+              ''
+        }}
       >
-        {options?.map((option) => (
+        {placeholder && (
+          <MenuItem disabled value=''>
+            <span className='text-gray'>{placeholder}</span>
+          </MenuItem>
+        )}
+
+        {localOptions?.map((option) => (
           <MenuItem value={option?.value}>
-            <ListItemText primary={option.name} />
+            {renderCheckbox && (
+              <StyledCheckbox
+                checked={selected?.join(',')?.indexOf(option.value) > -1}
+              />
+            )}
             {option?.label}
+
+            {/* {renderCheckbox && <ListItemText primary={option.label} />} */}
           </MenuItem>
         ))}
+        {customInput && (
+          <TextInput
+            className='flex items-center justify-center px-[16px]'
+            fullWidth
+            onKeyDown={(e) => handleAddMenuOptions(e)}
+            // value={textFieldValue}
+          />
+        )}
       </Select>
     </FormControl>
   )
